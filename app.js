17e8b464b167a21820193b337b0f498a7ed1823c
Shammi141 <jannatulshammi181@gmail.com>
2022-09-03 03:05:35 +0600

    category fixed

--- app.js
+++ app.js
@@ -1,11 +1,49 @@
-const loadNewses = async() =>{
+// category part 
+const loadCategories = async() =>{
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayCategories(data.data.news_category);
+}
+
+const displayCategories = categories =>{
+    const categoriesContainer = document.getElementById('category-names');
+    categories.forEach(category => {
+        const categoryDiv = document.createElement('div');
+        categoryDiv.innerHTML = `
+            <a class="category-link" onclick ="clickedCategoryNews('${category.category_id}')">${category.category_name} </a>
+        `;
+        categoriesContainer.appendChild(categoryDiv);
+    })
+}
+
+const clickedCategoryNews = (categoryId) => {
+    //console.log(categoryId);
+    loadNewses(categoryId);
+}
+
+loadCategories();
+
+
+
+
+
+// all news part 
+const loadNewses = async (categoryId) =>{
     const url = `https://openapi.programming-hero.com/api/news/category/08`
     const res = await fetch(url)
     const data = await res.json()
-    displayNewses(data.data)
+    displayNewses(data.data, categoryId);
 }
 
-const displayNewses = newses =>{
+const displayNewses = (newses, categoryId) =>{
+    console.log(categoryId);
+
+    //display categoryId's matching news
+    // if (categoryId === ${newses.category_id}){
+
+    // }
+
     const newsesContainer = document.getElementById('news-container');
     newses.forEach(news => {
         const newsDiv = document.createElement('div');
@@ -17,24 +55,28 @@ const displayNewses = newses =>{
             <div class="col-md-9">
                 <div class="card-body">
                     <h5 class="card-title">${news.title}</h5>
-                    <p class="card-text">${news.details.slice(0,350)}</p>
+                    <p class="card-text">${news.details.slice(0,250)}</p>
                     
                     <div>
                         <div> 
                             <p></p>
                             <p></p>
                         </div>
-                        <div>
                         
-                        <p></p>
-                        </div>
+                        <div onclick = "showAllInfo()" class="btn btn-primary">Show All</div>
                     </div>
                 </div>
             </div>
-        
+
         `;
         newsesContainer.appendChild(newsDiv);
     });
 }
 
-loadNewses()
+const showAllInfo = 
+
+loadNewses();
+
+
+
+
