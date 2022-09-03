3e8b51a247badc134880852d01eb3d101fec519f
Shammi141 <jannatulshammi181@gmail.com>
2022-09-03 20:20:33 +0600

    category fixed again

--- app.js
+++ app.js
@@ -9,83 +9,67 @@ const loadCategories = async() =>{
 const displayCategories = categories =>{
     const categoriesContainer = document.getElementById('category-names');
     categories.forEach(category => {
-        const categoryDiv = document.createElement('div');
-        categoryDiv.innerHTML = `
-            <a class="category-link" onclick ="clickedCategoryNews('${category.category_id}')">${category.category_name} </a>
-        `;
-        categoriesContainer.appendChild(categoryDiv);
+        const categoryLi = document.createElement('li');
+        categoryLi.innerHTML = `
+        <a onclick = "newsDetails('${category.category_id}')">${category.category_name}</a>
+        `
+        categoriesContainer.appendChild(categoryLi);
     })
 }
 
-const clickedCategoryNews = (categoryId) => {
-    //console.log(categoryId);
-    loadNewses(categoryId);
+const newsDetails =async (categoryId) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayNewsDetails(data.data);
 }
 
-loadCategories();
 
+// display part 
+const displayNewsDetails = (newses) =>{
+    const newsDetails = document.getElementById('news-container');
+    newsDetails.innerHTML = ``;
+    newses.forEach(news =>{
 
-
-
-
-// all news part 
-const loadNewses = async (categoryId) =>{
-    const url = `https://openapi.programming-hero.com/api/news/category/08`
-    const res = await fetch(url)
-    const data = await res.json()
-    displayNewses(data.data, categoryId);
-}
-
-const displayNewses = (newses, categoryId) =>{
-    console.log(categoryId);
-
-    
-
-    const newsesContainer = document.getElementById('news-container');
-    newses.forEach(news => {
         const newsDiv = document.createElement('div');
-        newsDiv.classList.add('row');
+        newsDiv.classList.add('row')
         newsDiv.innerHTML = `
-            <div class="col-md-3">
+                <div class="col-md-3">
                 <img src="${news.image_url}" class="img-fluid rounded-start" alt="...">
             </div>
             <div class="col-md-9">
                 <div class="card-body">
                     <h5 class="card-title">${news.title}</h5>
-                    <p class="card-text">${news.details.slice(0,200)}</p>
+                    <p class="card-text">${news.details.slice(0, 210)}...</p>
                     
                     <div class="d-flex flex-row flex justify-content-between">
                         <div class="d-flex"> 
                             <img class="author-img" src="${news.author.img}" alt="...">
                             <div class= "d-flex flex-column ms-2">
-                                <span>${news.author.name}</span>
-                                <span>${news.author.published_date}</span>
+                                <span>${news.author.name ? news.author.name : 'No Name'}</span>
+                                <span>${news.author.published_date ? news.author.published_date : 'No Publish Date'}</span>
                             </div>
                         </div>
 
                         <div>
                             <i class ="fa-regular fa-eye"></i>
-                            <span>${news.total_view}</span>
+                            <span>${news.total_view ? news.total_view : 'No Views'} M</span>
                         </div>
 
-                        <div onclick = "showAllInfo()" class="btn btn-primary " data-bs-toggle="modal" data-bs-target="#newsDetailModal">Show Details</div>
+                        <div onclick = "showAllInfo()" class="btn btn-primary " data-bs-toggle="modal" data-bs-target="#newsDetailModal">See Details</div>
                     </div>
                 </div>
             </div>
-
         `;
-        newsesContainer.appendChild(newsDiv);
 
-
-    });
-
-    const showAllInfo = () => {
-
-    }
+        newsDetails.appendChild(newsDiv);
+    })
 }
 
 
-loadNewses();
+
+loadCategories();
+
 
 
 
